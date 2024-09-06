db9e878183985947f22a634d91428a49733f1a10
vanshdhameliya07 <168632893+vanshdhameliya07@users.noreply.github.com>
2024-09-06 17:38:56 +0530

    Update 2.c

--- final exam/2.c
+++ final exam/2.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
-main()
+int consonant(char ch)
 {
-    int no;
-    printf("enter no ");
-    scanf("%d", &no);
+
+    char consonant;
+
+    if ((ch >= 'a' && ch <= 'z') && !(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'))
+    {
+        return 0;
+    }
+    return 1;
 }
 
+int main()
+{
+    char ch;
 
+    printf("Enter a character: ");
+    scanf("%c", &ch);
 
+    if (consonant(ch))
+    {
+        printf("%c a consonant.\n", ch);
+    }
+    else
+    {
+        printf("%c not a consonant.\n", ch);
+    }
+}
